1af935a53a3574e0ec23bf08e95b92e3c8b6eac8
vinit <khollam.vinit@gmail.com>
2020-12-19 21:21:14 +0530

    added homepage

--- crafter/src/app/blog/blogform/blogform.component.ts
+++ crafter/src/app/blog/blogform/blogform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AngularFireStorage} from  '@angular/fire/storage';
 
 @Component({
   selector: 'app-blogform',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogformComponent implements OnInit {
 
-  constructor() { }
+  constructor(private AF:AngularFireStorage) { }
+ 
+  b_title=''
+     b_coverPhoto=''
+     b_desc=''
+     b_body=[
+       {
+        heading:'',
+        para:[{
+        text:''    
+        }],
+        image:''
+       }
+     ]
 
+     b_post=''
+    b_category=''
+   
   ngOnInit(): void {
+
   }
+
+  selectedFile:any=''
+  onchange(ev){
+    this.selectedFile = ev.target.files[0];
+   }
+
+  onSectionUpload(){
+    let userId=localStorage.getItem('id');
+   var filepath=`BlogImage/${userId}/Blogimage`;
+   const fileRef=this.AF.ref(filepath)
+
+   this.AF.upload(filepath,this.selectedFile).then((res)=>{
+    
+     fileRef.getDownloadURL().subscribe((url)=>{
+      console.log(url);
+   
+
+    } ,(err)=>{
+      console.log(err,'this is err')
+    })
+   }).catch(err => console.log(err))
+  }
+
 
 }
